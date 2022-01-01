64fdfe3d92dfe297d7b767d91825fad99b440d31
Bavatharany <46834658+BavatharanyM@users.noreply.github.com>
2022-01-01 22:38:54 +0530

    Update StaticDemo03.java

--- StaticBehavior/StaticDemo03.java
+++ StaticBehavior/StaticDemo03.java
@@ -1,1 +1,38 @@
 
+public class StaticDemo03{
+public static int count ;
+
+
+//static block
+static{
+System.out.println("1st static block : " +count);
+
+}
+
+//instance or non static block
+{
+count = count +5 ;
+System.out.println("1st non static block : " + count);
+
+}
+
+//static block 
+static{
+new StaticDemo03();
+System.out.println("2nd static block : " + count);
+}
+
+
+//static block 
+static{
+count = count + 30;
+System.out.println("3rd static block : " + count);
+}
+
+public StaticDemo03(){
+System.out.println("constructor " + count);
+}
+
+
+public static void main(String[] args){}
+}
